0266c55e3c279210cc84f146f08f5248dd9a783a
Rashid <rashidm@dckap.com>
2022-01-05 21:25:44 +0530

    quickview system config

---
+++ QuickView/Helper/Data.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace DCKAP\QuickView\Helper;
+
+class Data extends \Magento\Framework\App\Helper\AbstractHelper
+{
+    protected $scopeConfig;
+    /**
+     * Data constructor
+     * @param \Magento\Framework\App\Helper\Context $context
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     * @param $getSalableQuantityDataBySku
+     */
+
+    public function __construct(
+        \Magento\Framework\App\Helper\Context $context,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig)
+    {
+        parent::__construct($context);
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    /**
+     * @return $telephoneConfig
+     */
+    public function getEnable()
+    {
+        $enableConfig = $this->scopeConfig->getValue(
+            'quickview/general/enable',
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
+        return $enableConfig;
+    }
+
+}
